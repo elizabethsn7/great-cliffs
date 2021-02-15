b15b552940849beada2c6033a3db01dee9512580
Elizabeth Kipp <elizabethkipp@MacBook-Pro.lan1>
2021-02-15 14:18:15 -0600

    removed partners code from scripts.js

--- Day3/js/scripts.js
+++ Day3/js/scripts.js
@@ -1,30 +1,3 @@
-var photos = []; //Declare an empty array to store image element
-var fileNames = []; //Declare an empty element to store image file names
-var imageList = []; //Declare an empty array to store html list that contain an image
-var attr = [
-  "Partner Bus Tours",
-  "Partner Cabin Rental",
-  "Partner Camping Adventure",
-  "Partner College Tours",
-  "Partner Bike Rentals",
-  "Partner Tour Group",
-];
-var image; //Declare an empty variable to store the assembled image list codes
-var openList = "<li class='partner'>"; //Declare a variable to contain open list tag
-var closeList = "</li>"; //Declare a variable to contain an close list tag
-
-for (var i = 0; i < 6; i++) {
-  fileNames.push("partner-" + (i + 1)); //Create image file name and store in the array
-  photos.push(
-    "<img src='images/partners/" + fileNames[i] + ".png' alt='" + attr[i] + "'>"
-  ); //Assemble file name into image element and store in an array
-  image = openList + photos[i] + closeList; //Assemble the image element from the array with list element stored in variables
-  imageList.push(image); //Store(push) the assembled list codes into an array
-}
-
-//Display all six image codes stored in the array
-document.getElementById("partners").innerHTML = imageList.join(" ");
-
 //User interaction with table cells
 $(document).ready(function () {
   $("td").click(function () {
